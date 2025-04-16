751888768d46baf009dec9bd21fed097b2b48ab1
DevaKrishna07 <devakrishna1418@gmail.com>
2025-04-16 19:29:46 +0530

    gitignore file initialized

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Hero from '../components/Hero'
+
+function Home() {
+  return (
+    <div>
+        <Hero />
+      <h1>Home</h1>
+    </div>
+  )
+}
+
+export default Home
