e3c6017475135257b4f28e7980c7408d1aa13d1f
Devakrishna07 <135791446+Devakrishna07@users.noreply.github.com>
2025-06-15 11:26:22 +0530

    Update Homepage.jsx

--- Frontend/src/pages/Homepage.jsx
+++ Frontend/src/pages/Homepage.jsx
@@ -25,7 +25,7 @@ function Homepage() {
                 typeSpeed={100}
                 backSpeed={50}
                 loop
-                backDelay={20000}
+                backDelay={5000}
                 className="text-xl font-semibold text-white dark:text-[#948979] "
             />
         </div>
