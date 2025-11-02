9eb7b86506ec5f086461df41d1fca93081373efa
DevaKrishna07 <devakrishna1418@gmail.com>
2025-11-02 09:45:16 +0530

    initialized new

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,33 +1,9 @@
-import React, { useEffect } from 'react'
-import Homepage from './pages/Homepage'
-import About from './pages/About'
-import NavBar from './components/NavBar'
-import Services from './pages/Services'
-import Contact from './pages/Contact'
-import ToggleButton from './components/ToggleButton'
+import React from 'react'
 
 const App = () => {
-  const handleThemeChange = (theme) =>{
-    const root = window.document.documentElement;
-    if(theme === "dark"){
-      root.classList.add("dark");
-    }else{
-      root.classList.remove("dark");
-    }
-  };
-
-  useEffect(() => {
-    const savedTheme = localStorage.getItem("theme") || "light";
-    handleThemeChange(savedTheme);
-  },[]);
-
   return (
-    <div className="scroll-smooth">
-      <NavBar onThemeChange={handleThemeChange} />
-      <Homepage />
-      <About />
-      <Services />
-      <Contact />
+    <div>
+      hlo
     </div>
   )
 }
