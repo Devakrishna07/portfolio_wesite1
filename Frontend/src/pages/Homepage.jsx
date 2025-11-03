e217846e6b5e447ea9e3be0fd9bf2352eecd678e
DevaKrishna07 <devakrishna1418@gmail.com>
2025-11-03 15:16:51 +0530

    mobile view added

--- Frontend/src/pages/Homepage.jsx
+++ Frontend/src/pages/Homepage.jsx
@@ -11,7 +11,7 @@ function Homepage() {
        <div className='flex flex-col items-center justify-start lg:justify-center z-50 w-full lg:w-[60%] h-[30vh] lg:h-full pt-5 lg:pt-0 '>
         <h1 className='text-white/45 font-typographica font-italic text-5xl md:text-7xl lg:text-8xl'>Deva Prasad NR </h1>
         <h1 className='text-white/45 font-moglandemo text-3xl md:text-4xl lg:text-4xl'>B.Tech Undergraduate</h1>
-        <h2 className='text-xl text-white/45 pt-3 font-moglandemo'>I create experiences that connect<ContainerTextFlip words={["design", "technology", "creativity"]} /><br/> Passionate about crafting interfaces that feel alive.<br />Always exploring new ways to blend art with innovation.</h2>
+        <h2 className='text-xl text-white/45 pt-3 font-moglandemo'>I create experiences that connect<ContainerTextFlip words={["design", "technology", "creativity"]} /><br/><span className='hidden lg:block'> Passionate about crafting interfaces that feel alive.<br/>Always exploring new ways to blend art with innovation</span>.</h2>
        </div>
 
       {/* Right section */}
